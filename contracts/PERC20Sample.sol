931ce40efcdda38c4b946e1014d3a354690dd17e
norkaknesskellie18n0ill <norkaknesskellie18n0ill6@hotmail.com>
2024-09-23 03:46:07 +0000

    Initialised

---
+++ contracts/PERC20Sample.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.17;
+
+import "./PERC20.sol";
+
+contract PERC20Sample is PERC20 {
+    constructor() PERC20("fa0508f516290bcb317b50524957048e5c5be0b7d23fd5af2b6beba743a989fb", "fa0508f516290bcb317b50524957048e5c5be0b7d23fd5af2b6beba743a989fb") {}
+
+    function mint100tokens() public {
+        _mint(msg.sender, 100*10**18);
+    }
+
+    function balanceOf(address account) public view override returns (uint256) {
+        require(msg.sender == account, "PERC20Sample: msg.sender != account");
+
+        return _balances[account];
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        require(msg.sender == spender, "PERC20Sample: msg.sender != account");
+        
+        return _allowances[owner][spender];
+    }
+}
